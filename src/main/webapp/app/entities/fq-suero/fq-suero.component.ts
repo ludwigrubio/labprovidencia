601379bb9e09246534528b80a8742a602b2dccc2
ludwigrubio <gerardo.lgrj@gmail.com>
2020-10-02 09:22:39 -0500

    Adding filters to all FQ and PruebaMicro

--- src/main/webapp/app/entities/fq-suero/fq-suero.component.ts
+++ src/main/webapp/app/entities/fq-suero/fq-suero.component.ts
@@ -11,6 +11,21 @@ import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
 import { FQSueroService } from './fq-suero.service';
 import { FQSueroDeleteDialogComponent } from './fq-suero-delete-dialog.component';
 
+// Filter added
+import { IArea } from 'app/shared/model/area.model';
+import { AreaService } from 'app/entities/area/area.service';
+import { IProducto } from 'app/shared/model/producto.model';
+import { ProductoService } from 'app/entities/producto/producto.service';
+import { IUserExtra } from 'app/shared/model/user-extra.model';
+import { UserExtraService } from 'app/entities/user-extra/user-extra.service';
+import { IPersonal } from 'app/shared/model/personal.model';
+import { PersonalService } from 'app/entities/personal/personal.service';
+import { Observable } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+import { distinctUntilChanged } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'jhi-fq-suero',
   templateUrl: './fq-suero.component.html',
@@ -24,23 +39,45 @@ export class FQSueroComponent implements OnInit, OnDestroy {
   predicate!: string;
   ascending!: boolean;
   ngbPaginationPage = 1;
+  filters = { lote: '', fecha: '', area: { id: '' }, analista: { id: '' }, proveedor: { id: '' }, producto: { id: '' } };
+  dateTop!: string;
+  dateButton!: string;
 
   constructor(
     protected fQSueroService: FQSueroService,
     protected activatedRoute: ActivatedRoute,
     protected router: Router,
     protected eventManager: JhiEventManager,
-    protected modalService: NgbModal
+    protected modalService: NgbModal,
+    protected areaService: AreaService,
+    protected userExtraService: UserExtraService,
+    protected productoService: ProductoService,
+    protected personalService: PersonalService
   ) {}
 
   loadPage(page?: number, dontNavigate?: boolean): void {
     const pageToLoad: number = page || this.page || 1;
+
+    if (this.filters.fecha !== '') {
+      this.dateTop = new Date(this.filters.fecha + ' 00:00:00').toISOString();
+      this.dateButton = new Date(this.filters.fecha + ' 23:59:59').toISOString();
+    } else {
+      this.dateTop = '';
+      this.dateButton = '';
+    }
 
     this.fQSueroService
       .query({
         page: pageToLoad - 1,
         size: this.itemsPerPage,
         sort: this.sort(),
+        'lote.contains': this.filters.lote || '',
+        'fecha.greaterThanOrEqual': this.dateTop || '',
+        'fecha.lessThanOrEqual': this.dateButton || '',
+        'areaId.equals': this.filters.area.id || '',
+        'analistaId.equals': this.filters.analista.id || '',
+        'proveedorId.equals': this.filters.proveedor.id || '',
+        'productoId.equals': this.filters.producto.id || '',
       })
       .subscribe(
         (res: HttpResponse<IFQSuero[]>) => this.onSuccess(res.body, res.headers, pageToLoad, !dontNavigate),
@@ -115,4 +152,44 @@ export class FQSueroComponent implements OnInit, OnDestroy {
   protected onError(): void {
     this.ngbPaginationPage = this.page ?? 1;
   }
+
+  formatterArea = (x: { area: string }) => x.area;
+
+  searchArea = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(term => (term.length < 2 ? [] : this.areaService.query({ 'area.contains': term }))),
+      map((res: HttpResponse<IArea[]>) => res.body || [])
+    );
+
+  formatterAnalista = (x: { nombreCompleto: string }) => x.nombreCompleto;
+
+  searchAnalista = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(term => (term.length < 2 ? [] : this.userExtraService.query({ 'nombreCompleto.contains': term }))),
+      map((res: HttpResponse<IUserExtra[]>) => res.body || [])
+    );
+
+  formatterProveedor = (x: { nombre: string }) => x.nombre;
+
+  searchProveedor = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(term => (term.length < 2 ? [] : this.personalService.query({ 'nombre.contains': term, 'relacionId.equals': '2' }))),
+      map((res: HttpResponse<IPersonal[]>) => res.body || [])
+    );
+
+  formatterProducto = (x: { producto: string }) => x.producto;
+
+  searchProducto = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(term => (term.length < 2 ? [] : this.productoService.query({ 'producto.contains': term }))),
+      map((res: HttpResponse<IProducto[]>) => res.body || [])
+    );
 }
--- src/main/webapp/app/entities/prueba-micro/prueba-micro.component.ts
+++ src/main/webapp/app/entities/prueba-micro/prueba-micro.component.ts
@@ -11,6 +11,24 @@ import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
 import { PruebaMicroService } from './prueba-micro.service';
 import { PruebaMicroDeleteDialogComponent } from './prueba-micro-delete-dialog.component';
 
+// Filter added
+import { IArea } from 'app/shared/model/area.model';
+import { AreaService } from 'app/entities/area/area.service';
+import { IUserExtra } from 'app/shared/model/user-extra.model';
+import { UserExtraService } from 'app/entities/user-extra/user-extra.service';
+import { IPersonal } from 'app/shared/model/personal.model';
+import { PersonalService } from 'app/entities/personal/personal.service';
+import { ICultivo } from 'app/shared/model/cultivo.model';
+import { CultivoService } from 'app/entities/cultivo/cultivo.service';
+import { ISuperficie } from 'app/shared/model/superficie.model';
+import { SuperficieService } from 'app/entities/superficie/superficie.service';
+import { Observable } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+import { distinctUntilChanged } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+
+import * as moment from 'moment'; // add this 1 of 4
 @Component({
   selector: 'jhi-prueba-micro',
   templateUrl: './prueba-micro.component.html',
@@ -24,23 +42,64 @@ export class PruebaMicroComponent implements OnInit, OnDestroy {
   predicate!: string;
   ascending!: boolean;
   ngbPaginationPage = 1;
+  filters = {
+    lote: '',
+    tipodeMuestra: '',
+    area: { id: '' },
+    analista: { id: '' },
+    proveedor: { id: '' },
+    cultivo: { id: '' },
+    superficie: { id: '' },
+    fechaInicio: '',
+    fechaFin: '',
+  };
+  dateTop!: string;
+  dateButton!: string;
 
   constructor(
     protected pruebaMicroService: PruebaMicroService,
     protected activatedRoute: ActivatedRoute,
     protected router: Router,
     protected eventManager: JhiEventManager,
-    protected modalService: NgbModal
+    protected modalService: NgbModal,
+    protected areaService: AreaService,
+    protected userExtraService: UserExtraService,
+    protected personalService: PersonalService,
+    protected cultivoService: CultivoService,
+    protected superficieService: SuperficieService
   ) {}
 
   loadPage(page?: number, dontNavigate?: boolean): void {
     const pageToLoad: number = page || this.page || 1;
+    const isValidDateI = moment(this.filters.fechaInicio, 'YYYY-MM-DD', true).isValid();
+    const isValidDateF = moment(this.filters.fechaFin, 'YYYY-MM-DD', true).isValid();
+
+    if (this.filters.fechaInicio !== '' && isValidDateI) {
+      this.dateTop = new Date(this.filters.fechaInicio + ' 00:00:00').toISOString();
+    } else {
+      this.dateTop = '';
+    }
+
+    if (this.filters.fechaFin !== '' && isValidDateF) {
+      this.dateButton = new Date(this.filters.fechaFin + ' 23:59:59').toISOString();
+    } else {
+      this.dateButton = '';
+    }
 
     this.pruebaMicroService
       .query({
         page: pageToLoad - 1,
         size: this.itemsPerPage,
         sort: this.sort(),
+        'lote.contains': this.filters.lote || '',
+        'tipodeMuestra.equals': this.filters.tipodeMuestra || '',
+        'areaId.equals': this.filters.area.id || '',
+        'analistaId.equals': this.filters.analista.id || '',
+        'proveedorId.equals': this.filters.proveedor.id || '',
+        'cultivoId.equals': this.filters.cultivo.id || '',
+        'superficieId.equals': this.filters.superficie.id || '',
+        'inicio.greaterThanOrEqual': this.dateTop || '',
+        'fin.lessThanOrEqual': this.dateButton || '',
       })
       .subscribe(
         (res: HttpResponse<IPruebaMicro[]>) => this.onSuccess(res.body, res.headers, pageToLoad, !dontNavigate),
@@ -115,4 +174,54 @@ export class PruebaMicroComponent implements OnInit, OnDestroy {
   protected onError(): void {
     this.ngbPaginationPage = this.page ?? 1;
   }
+
+  formatterArea = (x: { area: string }) => x.area;
+
+  searchArea = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(term => (term.length < 2 ? [] : this.areaService.query({ 'area.contains': term }))),
+      map((res: HttpResponse<IArea[]>) => res.body || [])
+    );
+
+  formatterAnalista = (x: { nombreCompleto: string }) => x.nombreCompleto;
+
+  searchAnalista = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(term => (term.length < 2 ? [] : this.userExtraService.query({ 'nombreCompleto.contains': term }))),
+      map((res: HttpResponse<IUserExtra[]>) => res.body || [])
+    );
+
+  formatterProveedor = (x: { nombre: string }) => x.nombre;
+
+  searchProveedor = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(term => (term.length < 2 ? [] : this.personalService.query({ 'nombre.contains': term, 'relacionId.equals': '2' }))),
+      map((res: HttpResponse<IPersonal[]>) => res.body || [])
+    );
+
+  formatterCultivo = (x: { cultivo: string }) => x.cultivo;
+
+  searchCultivo = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(term => (term.length < 2 ? [] : this.cultivoService.query({ 'cultivo.contains': term }))),
+      map((res: HttpResponse<ICultivo[]>) => res.body || [])
+    );
+
+  formatterSuperficie = (x: { superficie: string }) => x.superficie;
+
+  searchSuperficie = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(term => (term.length < 2 ? [] : this.superficieService.query({ 'superficie.contains': term }))),
+      map((res: HttpResponse<ISuperficie[]>) => res.body || [])
+    );
 }
